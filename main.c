f7c2722004ce6944a72d50bd87cac989141f5b20
bonbon08 <mail@jan-schmitt.eu>
2026-02-10 17:21:33 +0100

    changed to chicken picture

--- main.c
+++ main.c
@@ -7,28 +7,42 @@ int main(void)
 
     InitWindow(screenWidth, screenHeight, "Jumper Chicken");
 
+    Image chickenImage = LoadImage("Chicken.png");
+    ImageResize(&chickenImage, 64, 64);
+
+    Texture2D chickenTexture = LoadTextureFromImage(chickenImage);
+    UnloadImage(chickenImage); 
+
     Vector2 chickenPosition = { (float)screenWidth/2, (float)screenHeight/2 };
-    int Jumpframes = 0;
+    int jumpFrames = 0;
+
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {   
-        if (IsKeyDown(KEY_X)) {
-            Jumpframes = 10;
-        };
+        if (IsKeyPressed(KEY_X)) {  
+            jumpFrames = 12;
+        }
 
-        if (Jumpframes >= 1) {
+        if (jumpFrames > 0) {
             chickenPosition.y -= 5.0f;
-            Jumpframes -= 1;
+            jumpFrames--;
         } else {
-            chickenPosition.y += 5.0f;
-        };
+            chickenPosition.y += 3.0f;
+        }
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawCircleV(chickenPosition, 30.0f, MAROON);
+
+        DrawTexture(chickenTexture, 
+                    (int)chickenPosition.x, 
+                    (int)chickenPosition.y, 
+                    WHITE);
+
         EndDrawing();
     }
 
+    UnloadTexture(chickenTexture);
     CloseWindow();
     return 0;
 }
